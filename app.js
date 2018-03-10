aa6182d4826690f67ce9193ee0184998961e4458
Sean <samclaughlin@email.neit.edu>
2018-03-09 19:18:54 -0500

    final exam final product

--- app.js
+++ app.js
@@ -7,16 +7,14 @@ var cookieParser = require('cookie-parser');
 var passport = require('passport');
 var session = require('express-session');
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://Sean:1234@ds011745.mlab.com:11745/gameslib";
+var assert = require('assert');
+var url = "mongodb://Sean:passW0rd@ds235768.mlab.com:35768/ropeup";
+var currUser = null;
 
 var app = express();
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set("view engine", "ejs");
-
-//var entries = [];
-
-//app.locals.entries = entries;
 
 app.use(logger("dev"));
 
@@ -30,6 +28,11 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(function (req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  next();
+});
 
 passport.serializeUser(function(user, done){
 	done(null, user);
@@ -48,7 +51,7 @@ passport.use(new LocalStrategy({
 		MongoClient.connect(url, function(err, db){
 			if(err) throw err;
 			
-			var dbObj = db.db("gameslib");
+			var dbObj = db.db("ropeup");
 			
 			dbObj.collection("users").findOne({username:username}, function(err, results){
 				if(results.password === password){
@@ -67,10 +70,22 @@ function ensureAuthenticated(request, response, next){
 		next();
 	}
 	else{
-		response.redirect("/sign-in");
+		response.redirect("/sign-up");
 	}
 }
-	
+
+app.get("/sign-in", function(req, res){
+	res.render("sign-in");
+});
+
+app.get("/sign-up", function(req, res){
+	res.render("sign-up");
+});
+
+app.get("/profilePage"), function(req, res){
+    res.render("profilePage");
+}
+
 app.get("/logout", function(request, response){
 	request.logout();
 	response.redirect("/sign-in");
@@ -80,41 +95,76 @@ app.get("/", ensureAuthenticated, function(req, res){
 	MongoClient.connect(url, function(err, db){
 		if(err) throw err;
 		
-		var dbObj = db.db("gameslib");
+		var dbObj = db.db("ropeup");
 		
-		dbObj.collection("games").find().toArray(function(err, results){
-			console.log("Site served");
+		dbObj.collection("gyms").find().toArray(function(err, results){
 			db.close();
-			res.render("index",{games:results});
+			res.render("index",{gym:results});
 		});
 	});
 	
 });
 
 app.get("/new-entry", ensureAuthenticated, function(req, res){
-	res.render("new-entry");
-});
-
-app.get("/sign-in", function(req, res){
-	res.render("sign-in");
-});
-
-app.post("/new-entry", function(req, response){
-	if(!req.body.title || !req.body.body){
-		response.status(400).send("Entries must have some text!");
-		return;
-	}
-	
 	MongoClient.connect(url, function(err, db){
 		if(err) throw err;
 		
-		var dbObj = db.db("gameslib");
+		var dbObj = db.db("ropeup");
 		
-		dbObj.collection("games").save(req.body, function(err, result){
-			console.log("Data saved");
+		dbObj.collection("gyms").find().toArray(function(err, results){
+			console.log("Site served");
 			db.close();
-			response.redirect("/");
+			res.render("new-entry",{gyms:results});
 		});
+	});
+});
+
+app.get("/profilePage", ensureAuthenticated, function(request, response){
+    
+    MongoClient.connect(url, function(err, db){
+		if(err) throw err;
+		 
+		var dbObj = db.db("ropeup");
+        
+
+		
+		dbObj.collection("userGyms").find({username:request.user.username}).toArray(function(err, results){
+			db.close();
+			response.render("profilePage",{gyms:results, name:request.user.username});
+		});
+        
+	});
+		
+});
+
+app.get("/matches", ensureAuthenticated, function(request, response){
+    var searchParams;
+    
+    MongoClient.connect(url, function(err, db){
+		if(err) throw err;
+
+		var dbObj = db.db("ropeup");
+        
+        var params = dbObj.collection("userGyms").findOne({username:request.user.username}).then(function(params){
+            console.log(params);
+        
+		dbObj.collection("userGyms").find({$and: [{username:{$ne: request.user.username}}, {name: params.name},
+                                            {$or:[{$and:[{monday: params.monday}, {monday: "M"}]},
+                                                 {$and:[{tuesday: params.tuesday}, {tuesday: "T"}]},
+                                                 {$and:[{wednesday: params.wednesday}, {wednesday: "W"}]},
+                                                 {$and:[{thursday: params.thursday}, {thursday: "TH"}]},
+                                                 {$and:[{friday: params.friday}, {friday: "F"}]},
+                                                 {$and:[{saturday: params.saturday}, {saturday: "SA"}]},
+                                                 {$and:[{sunday: params.sunday}, {sunday: "SU"}]}]},
+                                            {$or:[{$and:[{sTime:{$gte: params.sTime}}, {sTime:{$lt: params.eTime}}]},
+                                                 {$and:[{eTime:{$gte: params.sTime}}, {eTime:{$lt: params.eTime}}]},
+                                                 {$and:[{sTime:{$lte: params.sTime}}, {eTime:{$gte: params.eTime}}]}]}
+                                                 ]}).toArray(function(err, results){
+			db.close();
+			response.render("matches",{gyms:results});
+
+		  });        
+        });     
 	});
 });
 
@@ -122,39 +172,91 @@ app.post("/sign-up", function(request, response){
 	console.log(request.body);
 	MongoClient.connect(url, function(err, db){
 		if(err) throw err;
+        
 		
-		var dbObj = db.db("gameslib");
+		var dbObj = db.db("ropeup");
 		
 		var user = {
-			username: request.body.username,
-			password: request.body.password
+                "username": request.body.username,
+                "password": request.body.password,
+                "passwordVer": request.body.passwordVer,
+                "email": request.body.email,
+                "fName": request.body.f_name,
+                "lName": request.body.l_name,
+                "sex": request.body.Sex,
+                "birthday": request.body.birthday
 		};
-		
-		dbObj.collection("users").insert(user, function(err, results){
-			if(err) throw err;
-			
-			request.login(request.body, function(){
-			response.redirect("/profile");
-			});
-		});
+        
+        //Make the entire object a string then  
+        //parse the string so you can access individual parts of the object.
+        var obj = JSON.parse(JSON.stringify(user));
+
+        //Check that what the user entered as a password re-entry mathches the origional password.
+        if(obj.password == obj.passwordVer)
+        { 
+            dbObj.collection("users").insert(user, function(err, results){
+			 if(err) throw err;
+                
+			 request.login(request.body, function(){
+			 response.redirect("/profilePage");
+                console.log("Data Saved");
+                });
+		      });
+        }
+        else
+        {
+            response.redirect("/sign-up");
+        }
+            
 	});
+});
+
+app.post("/new-entry", function(request, response){
+   MongoClient.connect(url, function(err, db){
+       if(err) throw err;
+       
+       dbObj = db.db("ropeup");
+       
+       var gymData = {
+           "username": request.user.username,
+           "name" : request.body.Name,
+           "monday" : request.body.monday,
+           "tuesday" : request.body.tuesday,
+           "wednesday" : request.body.wednesday,
+           "thursday" : request.body.thursday,
+           "friday" : request.body.friday,
+           "saturday": request.body.saturday,
+           "sunday": request.body.sunday,
+           "sTime" : request.body.start,
+           "eTime" : request.body.end
+       };
+             
+        var obj = JSON.parse(JSON.stringify(gymData));
+ 
+            //If atleast one day has been selected then send the data to the database.
+            if(obj.monday != "null" || obj.tuesday != "null" || obj.wednesday != "null" ||
+               obj.thursday != "null" || obj.friday != "null" || obj.saturday != "null" || obj.sunday != "null")
+            {
+                dbObj.collection("userGyms").insert(gymData, function(err, results){
+                    if(err) throw err;
+           
+                    db.close();
+                    response.redirect("profilePage");
+                });
+            }
+   });
 });
 
 app.post("/sign-in", passport.authenticate("local", {
 	failureRedirect:"/sign-in",
 	}), function(request, response){
-		response.redirect("/");
+		response.redirect("profilePage");
 	});
-
-
-app.get("/profile", function(request, response){
-		response.json(request.user);
-});
 
 app.use(function(req, res){
 	res.status(404).render("404");
 });
 
-http.createServer(app).listen(8081, function(){
-	console.log("Game libray server started on port 3000");
+http.createServer(app).listen(3000, function(){
+	console.log("server started on port 3000");
 });
